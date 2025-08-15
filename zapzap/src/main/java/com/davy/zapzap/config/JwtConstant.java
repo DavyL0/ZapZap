5a4f8888046d3d0ec62490ab53ffb091594eed3d
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-08-15 13:55:35 -0300

    feat: create JwtConstant.java
    JWT constants

---
+++ zapzap/src/main/java/com/davy/zapzap/config/JwtConstant.java
@@ -0,0 +1,17 @@
+package com.davy.zapzap.config;
+
+/**
+ * Project: ZapZap
+ * Package: com.davy.zapzap.config
+ * <p>
+ * User: MegaD
+ * Email: davylopes866@gmail.com
+ * Date: 15/08/2025
+ * Time: 13:51
+ * <p>
+ */
+public class JwtConstant {
+
+    public static final String JWT_HEADER = "Authorization";
+    public static final String SECRET_KEY = "oBZLAzwKpEO0DBIIsAAr5T7PNHNXxLyJ";
+}
