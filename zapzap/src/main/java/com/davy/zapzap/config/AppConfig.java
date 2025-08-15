68a65938e69b64e2dfeb353eb7b77870dcec8be5
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-08-15 13:00:49 -0300

    feat: add spring security config

---
+++ zapzap/src/main/java/com/davy/zapzap/config/AppConfig.java
@@ -0,0 +1,47 @@
+package com.davy.zapzap.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Project: ZapZap
+ * Package: com.davy.zapzap.config
+ * <p>
+ * User: MegaD
+ * Email: davylopes866@gmail.com
+ * Date: 15/08/2025
+ * Time: 12:27
+ * <p>
+ */
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration cfg = new CorsConfiguration();
+                    cfg.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                    cfg.setAllowedMethods(Collections.singletonList("*"));
+                    cfg.setAllowCredentials(true);
+                    cfg.setAllowedHeaders(Collections.singletonList("*"));
+                    cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                    cfg.setMaxAge(3600L);
+                    return cfg;
+                }))
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable());
+
+        return http.build();
+    }
+}
